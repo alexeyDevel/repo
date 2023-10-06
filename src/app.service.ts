1e73ef640595f3d67ee87ca93d9c0230f6d20eb8
alexeyDevel <alexeydevelopermm@gmail.com>
2023-10-06 19:36:49 +0300

    push

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      name: 'Vercel deoploy',
+      message: 'Готово',
+    };
   }
 }
