2edf60ce2be3681d37f688e072308be95befaab6
alexeyDevel <alexeydevelopermm@gmail.com>
2024-01-05 10:49:03 +0300

    dev

--- src/main.ts
+++ src/main.ts
@@ -4,49 +4,33 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
-import { resolve } from 'path';
-import { writeFileSync, createWriteStream } from 'fs';
-import { get } from 'http';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+ const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.use(cookieParser());
-  app.use(
+ app.use(cookieParser());
+ app.use(
     session({
       secret: 'your_secret_key',
       resave: false,
       saveUninitialized: false,
     }),
-  );
-  app.enableShutdownHooks();
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
-  // app.useGlobalFilters(new CommonExceptionFilter());
-  const options = new DocumentBuilder()
+ );
+ app.enableShutdownHooks();
+ const { httpAdapter } = app.get(HttpAdapterHost);
+ app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+ // app.useGlobalFilters(new CommonExceptionFilter());
+ const options = new DocumentBuilder()
     .setTitle('Sovtrud API')
     .setDescription('The sovtrud API description')
     .setVersion('1.0')
     .addTag('sovtrud')
     .build();
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/swagger', app, document);
+ const document = SwaggerModule.createDocument(app, options);
+ SwaggerModule.setup('/swagger', app, document);
 
-  if (process.env.NODE_ENV === 'development') {
-    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
-
-    // write swagger json file
-    const pathToSwaggerJson = resolve(
-      pathToSwaggerStaticFolder,
-      'swagger.json',
-    );
-    const swaggerJson = JSON.stringify(document, null, 2);
-    writeFileSync(pathToSwaggerJson, swaggerJson);
-    console.log(`Swagger JSON file written to: '/swagger-static/swagger.json'`);
-  }
-
-  await app.listen(process.env.PORT || 3000);
+ await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
-
